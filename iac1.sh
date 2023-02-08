8cc09c2cfaa9f548dbcae62d811a2823718f94c0
Daniel-Santin <32037139@mackenzista.com.br>
2023-02-08 15:32:30 -0300

    .
    .

--- iac1.sh
+++ iac1.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo "Criando diretórios..."
+echo "Criando diretórios"
 
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
-echo "Criando grupos de usuários..."
+echo "Criando grupos de usuários"
 
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Criando usuários..."
+echo "Criando usuários"
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd daniel -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 
@@ -27,7 +27,7 @@ useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
-echo "Especificando permissões dos diretórios...."
+echo "Especificando permissões dos diretórios"
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
@@ -38,5 +38,5 @@ chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico
 
-echo "Fim....."
+echo "Fim"
 
---
+++ iac2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DIRETORIOS=("publico" "ven" "sec" "adm")
+GRP=("GRP_ADM" "GRP_VEN" "GRP_SEC")
+USRS=("daniel" "maria" "joao" "debora" "sebastiana" "roberto" "josefina" "rogerio")
+
+
+echo "Criando diretorios"
+
+for pastas in "${DIRETORIOS[@]}";do
+	mkdir /$pastas
+done
+
+echo "Criando grupos de usuarios"
+
+for gr in ${GRP[@]};do
+       groupadd $gr	
+done
+
+echo "Criando usuarios"
+
+for us in ${USRS[@]};do
+       useradd $us -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+done
+
+
+
+echo "Especificando permissões"
+
+chown root:GRP_ADM /adm
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Fim ......"
